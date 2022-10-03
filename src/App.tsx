28318d695da346108efa2343bdd854ccc1c5b3d0
fadhilrafiii <fadhilrafi90@gmail.com>
2022-10-03 13:50:43 +0700

    Move sidebar to private route

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,7 @@ import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Loading from 'Components/LoadingPage';
-import Navbar from 'Components/Navbar';
 import { PrivateRoute, PublicRoute } from 'Components/Router';
-import Sidebar from 'Components/Sidebar';
 
 const Login = lazy(() => import('Pages/Login'));
 const Register = lazy(() => import('Pages/Register'));
@@ -16,25 +14,17 @@ const App = () => {
   return (
     <BrowserRouter>
       <Suspense fallback={<Loading />}>
-        <div className="app-container">
-          <Sidebar />
-          <div className="page-container">
-            <Navbar />
-            <div className="page-wrapper">
-              <Routes>
-                <Route element={<PublicRoute />}>
-                  <Route path="/" element={<Login />} />
-                  <Route path="/register" element={<Register />} />
-                </Route>
-                <Route element={<PrivateRoute />}>
-                  <Route path="/to-do" element={<ToDo />} />
-                  <Route path="/finance" element={<UnderConstruction title="Finance" />} />
-                  <Route path="/chats" element={<UnderConstruction title="Chats" />} />
-                </Route>
-              </Routes>
-            </div>
-          </div>
-        </div>
+        <Routes>
+          <Route element={<PublicRoute />}>
+            <Route path="/" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Route>
+          <Route element={<PrivateRoute />}>
+            <Route path="/to-do" element={<ToDo />} />
+            <Route path="/finance" element={<UnderConstruction title="Finance" />} />
+            <Route path="/chats" element={<UnderConstruction title="Chats" />} />
+          </Route>
+        </Routes>
       </Suspense>
     </BrowserRouter>
   );
--- src/Components/Router/PrivateRoute.tsx
+++ src/Components/Router/PrivateRoute.tsx
@@ -3,10 +3,15 @@ import React, { useCallback, useEffect } from 'react';
 import { AxiosResponse } from 'axios';
 import { Outlet, useNavigate } from 'react-router-dom';
 
+import Navbar from 'Components/Navbar';
+import Sidebar from 'Components/Sidebar';
+
 import { useAppDispatch, useAppSelector } from 'Redux/hooks';
 import { setAuthLoading, setUser, userSelector } from 'Redux/Slices/userSlice';
 
-import { authenticateAPI } from 'Clients/auth';
+import { authenticateAPI } from 'Clients/auth/authenticate';
+
+import styles from './index.module.css';
 
 const PrivateRoute = () => {
   const dispatch = useAppDispatch();
@@ -15,7 +20,7 @@ const PrivateRoute = () => {
 
   const authenticateUser = useCallback(async () => {
     await authenticateAPI()
-      .then(async (res: AxiosResponse) => dispatch(setUser(res.data.data)))
+      .then(async (res: AxiosResponse) => dispatch(setUser(res.data)))
       .catch(async () => {
         await dispatch(setAuthLoading(false));
         navigate('/');
@@ -28,7 +33,17 @@ const PrivateRoute = () => {
     }
   }, [authenticateUser, isAuthenticated]);
 
-  return <Outlet />;
+  return (
+    <div className={styles.pageWrapper}>
+      <Sidebar />
+      <div className={styles.pageContainer}>
+        <Navbar />
+        <div className={styles.pageContent}>
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PrivateRoute;
